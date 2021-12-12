db11732af5be6b4bee487b8db6e0044a8bfcae66
Ryan Wood <woodr1991@googlemail.com>
2021-12-12 08:22:10 +0000

    WaveConfig - 2 paths created for enemies to follow - basic enemy movement

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfigSO;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfigSO.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    private void FollowPath()
+    {
+        if(waypointIndex < waypoints.Count)    
+        {
+            Vector3 targetPosition = waypoints[waypointIndex].position;     //new vector3 of the postition of the current waypoint we want to move towards
+            float delta = waveConfigSO.GetMoveSpeed() * Time.deltaTime;     //float of how fast we want the ship to move
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);    //move the ship from its current position towars the next way point at the speed we've set
+            if(transform.position == targetPosition)    //if the position of the ship and the waypoint are the same add one to the waypoint index, to stat moveing towards the next waypoint
+            {
+                waypointIndex++;    //increment waypoint index
+            }
+        }
+        else
+        {
+            Destroy(gameObject);    //if there are no more wypoints destropy the ship
+        }
+    }
+}
